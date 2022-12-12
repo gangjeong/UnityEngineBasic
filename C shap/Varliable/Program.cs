44934e16e08b6b42724ae682927c9ceda0119e21
gangjeong <120103964+gangjeong@users.noreply.github.com>
2022-12-12 21:49:28 +0900

    firstProgram
    1.일단 보류

---
+++ C shap/Varliable/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Varliable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
