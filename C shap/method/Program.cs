d1e3179b4f8cbbde6b96f1bdfeb0b16722f31921
gangjeong <120103964+gangjeong@users.noreply.github.com>
2022-12-13 21:51:09 +0900

    Second Program
    멧허드 클래스 옵젝

---
+++ C shap/method/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+// Function : 연산을 수행할 수 있는 기능
+// Method : 객체/클래스 단위로 호출되는 Function (⊃ 멤버 함수)
+// Method ⊂ Function
+//
+// 함수 형태 3가지
+// 함수 선언, 정의, 호출
+// 함수 선언 : 아래 형태와같은 함수를 사용하겠다 라고만 선언하는 것이고, 실제 함수가 어떤 연샨을 하는지 구현부분이 없음.
+// 변환형 함수이름 (피라미티);
+//
+// 함수 정의 :
+// 변환형 함수이름 (피라미티)
+// {
+//      연산
+//      return;
+// }
+// 함수 호출 :
+// 함수 이름 (인자);
+namespace Method
+{
+    class Program
+    {
+        // 여기서 Main 함수는 함수 정의
+        static void Main (string[] args)
+        {
+            PrintHelloWorld();
+            string name = "dfdf";
+            PrintSomthing(name);
+        }
+
+        static void PrintHelloWorld()
+        {
+            // 여기 Console,writeLine 은 함수 호출
+            Console.WriteLine("Hello World!");
+        }
+        static void PrintSomthing (string something)
+        {
+            Console.WriteLine(something);
+        }
+
+    }
+}
