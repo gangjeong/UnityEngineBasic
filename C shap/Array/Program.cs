cb7dc9207feb9ab5883da99e85f7ab37be98c40d
gangjeong <120103964+gangjeong@users.noreply.github.com>
2022-12-16 21:44:58 +0900

    Fifth Program
    5번째 수업

---
+++ C shap/Array/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Arazy
+{
+    // 배열
+    // 동일한 자료형으로 연속적인 데이터 타입 (연속적인 : 메모리상에서 주소가 연속적으로 붙어있다.)
+    // 형태 :
+    // 자료형[]
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr_int;
+        }
+    }
+}
